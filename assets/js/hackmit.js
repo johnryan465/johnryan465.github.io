79152ecd4ba91b1f6da8655a86cb087ee0930c2a
John Ryan <johnryan465@gmail.com>
2018-07-07 15:05:14 +0100

    Update hackmit.js

--- assets/js/hackmit.js
+++ assets/js/hackmit.js
@@ -1,15 +1,1 @@
-var data = "username=johnryan465_a18a5c";
-
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4) {
-    console.log(this.responseText);
-  }
-});
-
-xhr.open("POST", "/beta/boost_rating");
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-xhr.send(data);
+$.post('/beta/api/boost_rating',{username:"johnryan465_a18a5c"});
