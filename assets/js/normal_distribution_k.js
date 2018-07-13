c2e6a213b60c1afad36c146fd11ebd70d160550a
John Ryan <johnryan465@gmail.com>
2018-07-13 15:12:41 +0100

    Update normal_distribution_k.js

--- assets/js/normal_distribution_k.js
+++ assets/js/normal_distribution_k.js
@@ -27,7 +27,7 @@ var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
-var svg = d3.select("#normal_distribution_k").append("svg")
+var svg = d3.select("#normal_distribution_k").selectAll("div").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
