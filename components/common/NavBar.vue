db3859943e605038614581ec6c31e6ae62a0a38b
johnryan465 <johnryan465@gmail.com>
2020-12-31 23:09:53 +0000

    NavBar colour

--- components/common/NavBar.vue
+++ components/common/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="bg-blue-300" app dense color="var(--colour-2)">
-    <div class="sm:block sm:ml-6">
+  <nav>
+    <div class="sm:block sm:ml-6 border-gray-400 border-b-2">
       <div class="flex items-center justify-between">
         <span class="hidden vs:inline-block text-black px-5">johnryan.tech</span>
         <div class="flex space-x-4">
@@ -8,8 +8,8 @@
             v-for="link in links"
             :key="link[0]"
             :to="link[1]"
-            exact-active-class="text-white"
-            class="bg-blue-300 hover:bg-blue-400 block px-2 sm:px-5 py-2 rounded-md text-base font-medium m-1"
+            exact-active-class="text-blue-300"
+            class="hover:text-blue-300 block px-2 sm:px-5 py-2 rounded-md text-base font-medium m-1"
           >
             {{ link[0] }}
           </nuxt-link>
