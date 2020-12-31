195566c80209e67c6bc2076831da183590b8e7cd
johnryan465 <johnryan465@gmail.com>
2020-12-31 21:49:31 +0000

    More responsive

--- components/common/NavBar.vue
+++ components/common/NavBar.vue
@@ -2,14 +2,14 @@
   <nav class="bg-blue-300" app dense color="var(--colour-2)">
     <div class="sm:block sm:ml-6">
       <div class="flex items-center justify-between">
-        <span class="inline-block text-black">johnryan.tech</span>
+        <span class="hidden vs:inline-block text-black px-5">johnryan.tech</span>
         <div class="flex space-x-4">
           <nuxt-link
             v-for="link in links"
             :key="link[0]"
             :to="link[1]"
             exact-active-class="text-white"
-            class="bg-blue-300 hover:bg-blue-400 block px-5 py-2 rounded-md text-base font-medium m-1"
+            class="bg-blue-300 hover:bg-blue-400 block px-2 sm:px-5 py-2 rounded-md text-base font-medium m-1"
           >
             {{ link[0] }}
           </nuxt-link>
