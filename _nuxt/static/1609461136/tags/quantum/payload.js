7c4fb7e5c97def6ce26067f661317b13beeff44d
johnryan465 <johnryan465@users.noreply.github.com>
2021-01-01 20:42:57 +0000

    deploy: johnryan465/johnryan465.github.io@88db33d3c84e75243e33e7af73831e06392a5f77

--- _nuxt/static/1609461136/tags/quantum/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/tags/quantum", (function(a){return {data:[{tag:{slug:"quantum",name:"Quantum",colour:"purple",toc:[],body:{type:"root",children:[]},dir:"\u002Ftags",path:"\u002Ftags\u002Fquantum",extension:".md",createdAt:a,updatedAt:a},articles:[]}],fetch:[],mutations:void 0}}("2021-01-01T00:31:20.112Z")));
